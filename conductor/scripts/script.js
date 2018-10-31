4407613d98c62c853a9c9c44edd1583d5535aacc
David Kam <davekam@davekam.com>
2018-10-31 16:50:51 -0500

    conductor prototype

---
+++ conductor/scripts/script.js
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Credit for these fireworks goes to: https://jsfiddle.net/dtrooper/AceJJ/
+ ******************************************************************************/
+var SCREEN_WIDTH = window.innerWidth,
+    SCREEN_HEIGHT = window.innerHeight,
+    mousePos = {
+        x: 400,
+        y: 300
+    },
+
+    // create canvas
+    canvas = document.createElement('canvas'),
+    context = canvas.getContext('2d'),
+    particles = [],
+    MAX_PARTICLES = 1000,
+    colorCode = 0;
+
+// init
+$(document).ready(function() {
+    document.body.appendChild(canvas);
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    setInterval(loop, 1000 / 50);
+});
+
+$(document).mousedown(function(e) {
+    var firework = new Firework(e.clientX, e.clientY, 120, (Math.random() * 60 + 120));
+    firework.explode();
+    $('#timpani').clone()[0].play();
+});
+
+function loop() {
+    // update screen size
+    if (SCREEN_WIDTH != window.innerWidth) {
+        canvas.width = SCREEN_WIDTH = window.innerWidth;
+    }
+    if (SCREEN_HEIGHT != window.innerHeight) {
+        canvas.height = SCREEN_HEIGHT = window.innerHeight;
+    }
+
+    // clear canvas
+    context.fillStyle = "rgba(0, 0, 0, 0.15)";
+    context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    var existingParticles = [];
+
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].update();
+
+        // render and save particles that can be rendered
+        if (particles[i].exists()) {
+            particles[i].render(context);
+            existingParticles.push(particles[i]);
+        }
+    }
+
+    // update array with existing particles - old particles should be garbage collected
+    particles = existingParticles;
+
+    while (particles.length > MAX_PARTICLES) {
+        particles.shift();
+    }
+}
